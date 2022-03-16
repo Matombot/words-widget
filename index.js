6d870a768493b6b8dcd473b07ccbfd08ae8f466a
tshifhiwa <matombot@gmail.com>
2022-03-16 22:13:54 +0200

    initial commit

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const sentenceElem = document.querySelector(".sentence");
 const wordCountElem = document.querySelector(".wordCount");
 const word_btn = document.querySelector(".word_btn");
 const displaySentence = document.querySelector(".displaySentence")
+const hideWords = document.querySelector(".hideWords")
 
 const wordBtnClicked = () => {
     let sentences = sentenceElem.value;
-//console.log(sentences);
+    //console.log(sentences);
     let word = sentences.split(" ")
-//let word = sentences.split(" ").trim();
+    //let word = sentences.split(" ").trim();
 
-    
-    const Words = word.map(words => {
+
+    let Words = word.map(words => {
         if (words.length >= 4) {
             words = `<mark>${words}</mark>`;
         }
@@ -23,12 +24,24 @@ const wordBtnClicked = () => {
         sentencee += Words[i] + " ";
 
     }
-    
-    
+
     displaySentence.innerHTML = sentencee;
-    //const
-   wordCountElem.innerHTML = word.length;
-  
+
+    wordCountElem.innerHTML = word.length;
+
+
+hideWords.addEventListener('click', function(){
+    let string = " ";
+    const longestWord =sentenceElem.value.split(" ");
+    for (let i = 0; i < longestWord.length; i++) {
+const longestWords = longestWord[i];
+if(longestWords.length >5){
+    string += "<mark>" + longestWords + "</mark>" + " "
+}
+
+    }
+    displaySentence.innerHTML =`display words more than five : ${string}`;
+})
 }
 
 
--- read.md
+++ read.md
@@ -1,4 +1,5 @@
 Words widget
+
 Create a widget that allows a user to enter a sentence of 5 or more words. When the user clicks a button the sentence should be analyzed.
 
 The widget:
@@ -10,9 +11,14 @@ Select a decent color sceme for your widget from coolors.
 ------------------------------------------------
 
 Hide & highlight
+
 There should be an option to hide all words shorter than 5 characters. Use a checkbox.
 
 Highlight the longest word/s in the sentence.
 
 -------------------------------------------------------------------------------
 
+Keep track
+Keep track of the last 5 sentences entered and show them on the screen. If you click on any of these sentences details for it should be shown as if it was the sentence that was typed in.
+
+Keep track of the average length of words in the last 5 sentences. Show for the latest sentence entered if it's average word length is higher or lower than the last 5 sentences by displaying a green dot if higher and an orange dot if lower.
