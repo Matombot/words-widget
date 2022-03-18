a5e2bf90c34e5192e9050adf55309b6197b28c64
tshifhiwa <matombot@gmail.com>
2022-03-18 14:01:22 +0200

    initial commit

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const word_btn = document.querySelector(".word_btn");
 const displaySentence = document.querySelector(".displaySentence")
 const hideWords = document.querySelector(".hideWords")
 const longest_word = document.querySelector(".longest_word")
+displaylastWords = document.querySelector(".last5Words")
 
 const wordBtnClicked = () => {
     let sentences = sentenceElem.value;
     //console.log(sentences);
     let word = sentences.split(" ")
     //let word = sentences.split(" ").trim();
+    let lastWords = [];
 
 
     let Words = word.map(words => {
@@ -41,23 +43,27 @@ hideWords.addEventListener('click', function(){
 const longestWords = longestWord[i];
 
 if (hideWords.checked === true){
-   if(longestWords.length >5 && longestWords.length>= longWord.length){
+   if(longestWords.length > 5 && longestWords.length>= longWord.length){
     string += `<mark style = 'color :red'>${longestWords}</mark>` + " "; 
     longWord = longestWords;
 
 }
 displaySentence.innerHTML =`display words that has more than five characters : ${string}`;
-
+longest_word.innerHTML = `longestword is displayed here: <mark style = 'color :green'> ${longWord} </mark>`;
+ 
   }
  else{displaySentence.innerHTML = sentencee;
 
  }
 
     }  
-longest_word.innerHTML = `longestword is displayed here: <mark style = 'color :green'> ${longWord} </mark>`;
- 
-})
 
+})
+if (lastWords.length < 5 ){
+    lastWords.push(sentences)
+}
+console.log(lastWords);
+displaylastWords.innerHTML = `${lastWords}`
 }
 
 word_btn.addEventListener('click', wordBtnClicked)
